5f24d885199730dcf52141cd34bbb6b1f6ea90d7
Danil Krutikov <kdanil.i@yandex.ru>
2023-04-05 20:56:10 +0300

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,2 @@
+def get_employees(employee_id):
+    return 'name, Department'
---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary(salary):
+    return f'размер исчисляемого налога НДФЛ={salary * 0.13}, на руки={salary * 0.87}'
